71b730a66478e25a206834a387c905279137c0c9
Lars Yndal Sørensen <yndal84@gmail.com>
2015-08-24 13:32:33 +0200

    Worked on the persons

--- src/Person.java
+++ src/Person.java
@@ -1,26 +1,53 @@
 abstract class Person {
-	int[] preferences;
-	int[] invertedPreferences;
+	final int[] preferences;
+	final int[] invertedPreferences;
 	int nextPrefered = 0;
-	int id;
-	String name;
-
+	final int id;
+	final String name;
+	//boolean isEngaged = false;
+	Person engagedTo;
+	
 	public Person(String name, int id, int[] preferences) {
 		this.id = id;
 		this.name = name;
 		this.preferences = preferences;
+		invertedPreferences = new int[preferences.length];
+		for(int i=0; i<preferences.length; i++)
+			invertedPreferences[preferences[i]] = i;
+	}
+	
+	public boolean isEngaged(){
+		return engagedTo != null; 
+	}
+	
+	public int getId(){
+		return id;
 	}
 }
 
 class Chick extends Person {
+	Bro engagedTo;
+	
 	public Chick(String name, int id, int[] preferences) {
 		super(name, id, preferences);
 	}
 	
 	public boolean acceptPropose(Bro bro){
+		if(nextPrefered < invertedPreferences[bro.getId()]){
+			nextPrefered = invertedPreferences[bro.getId()];
+			
+			divorce(engagedTo);
+			engagedTo = bro;
+			
+			return true;
+		}
 		
-		
-		return true;
+		return false;
+	}
+	
+	private void divorce(Bro bro){
+		bro.divorceFrom(this);
+		engagedTo = null;
 	}
 }
 
@@ -28,12 +55,22 @@ class Bro extends Person {
 	public Bro(String name, int id, int[] preferences) {
 		super(name, id, preferences);
 	}
-
+	
 	public int NextPreference() {
 		return preferences[nextPrefered++];
 	}
+	
+	public int getNextPrefered(){
+		return nextPrefered;
+	}
 
 	public void ProposeTo(Chick chick) {
-		chick.acceptPropose(this);
+		if(chick.acceptPropose(this)){
+			engagedTo = chick;
+		}
+	}
+	
+	public void divorceFrom(Chick chick){
+		engagedTo = null;
 	}
 }
