9db10cecf4642d2044eb0c75f12eb8aa07c2e658
Lars Yndal Sørensen <yndal84@gmail.com>
2015-08-24 10:37:57 +0200

    first commit...

---
+++ src/StableMatching.java
@@ -0,0 +1,45 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class StableMatching {
+	
+	public StableMatching(String path) throws Exception{
+		File file = new File(path);
+		readData(file);
+	}
+	
+	public void readData(File file) throws FileNotFoundException{
+		Scanner scanner = new Scanner(file);
+		
+		scanner;
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	public void main(String[] args) throws Exception{
+		String filepath = "input/sm-bbt-in.txt";
+		StableMatching sm = new StableMatching(filepath);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
