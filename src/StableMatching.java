9bff6b5fc0a71a2fc4c00971449780051070f347
Lars Yndal Sørensen <yndal84@gmail.com>
2015-08-24 11:28:28 +0200

    can read inputs

--- src/StableMatching.java
+++ src/StableMatching.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class StableMatching {
 	
+	int[] men;
+	int[] women;
+	
+	
 	public StableMatching(String path) throws Exception{
 		File file = new File(path);
 		readData(file);
@@ -16,8 +20,38 @@ public class StableMatching {
 		String str;
 		while((str = scanner.nextLine()).startsWith("#"));
 		
+		str = str.substring(str.indexOf('=')+1);
 		int n = Integer.valueOf(str);
 		System.out.println("n: " + n);
+		
+		men = new int[n];
+		women = new int[n];
+		
+		while(!(str = scanner.nextLine()).isEmpty()){
+			int index = str.indexOf(" ");
+			int number = Integer.valueOf(str.substring(0, index));
+			String name = str.substring(index + 1);
+			System.out.println("number: " + number + ", name: " + name);
+		}
+		
+		System.out.println("---------");
+		
+		while(scanner.hasNextLine() && !(str = scanner.nextLine()).isEmpty()){
+			int person = str.indexOf(":");
+			int personIndex = Integer.valueOf(str.substring(0, person));
+			String priorities = str.substring(str.indexOf(' ')+1);
+				for(int i=0; i<n; i++){
+					int index1 = priorities.indexOf(' ');
+					
+					int prio = Integer.valueOf(priorities.substring(0, index1 == -1 ? priorities.length() : index1));
+					priorities = priorities.substring(priorities.indexOf(' ')+1);
+					
+					System.out.print(prio + " ");
+				}
+				System.out.println();			
+		}
+		
+		
 		
 		
 		
