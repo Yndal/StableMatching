8746285bfe3e611c9c29afc48ed22f7ece6c231e
Lars Yndal Sørensen <yndal84@gmail.com>
2015-08-31 11:11:11 +0200

    algorithm implemented but untested

--- src/scheduling/Scheduling.java
+++ src/scheduling/Scheduling.java
@@ -5,16 +5,22 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
+import java.util.Stack;
 
 class Job {
 	private final int startTime;
 	private final int jobTime;
+	private final int id;
 
-	public Job(int start, int time){
+	public Job(int id, int start, int time){
+		this.id = id;
 		this.startTime = start;
 		this.jobTime = time;
 	}
 
+	public int getId(){
+		return id;
+	}
 	public int getStartTime() {
 		return startTime;
 	}
@@ -22,20 +28,29 @@ class Job {
 	public int getJobTime() {
 		return jobTime;
 	}
-
-
 }
 
 class Resource{
 	private final List<Job> jobs = new ArrayList<>();
 	private int currentEndTime = 0;
+	private final int thisResourceId;
+	private static int resourceCounter = 0;
 
+	public Resource(){
+		thisResourceId = resourceCounter++;
+	}
+	
 	public void addJob(Job job){
 		jobs.add(job);
+		currentEndTime += job.getJobTime();
 	}
 
 	public int getCurrentEndTime(){
 		return currentEndTime;
+	}
+	
+	public int getResourceNumber(){
+		return thisResourceId;
 	}
 }
 
@@ -53,19 +68,52 @@ public class Scheduling {
 		}
 	});
 	private List<Resource> resources;
+	private List<Job> unassignedJobs;
+	private Stack<Job> jobStack;
 	
 	public Scheduling(File file){
 
 	}
 
 	private void readData(){
+		resources = new ArrayList<>();
+		unassignedJobs = new ArrayList<>();
+		jobStack = new Stack<>();
 
 	}
 
-	private void solve(){
-		resources = new ArrayList<>();
+	public void solve(){
+		while(!pq.isEmpty()){
+			Job job = pq.poll();
+			boolean assigned = false;
+			for(int i=0; i<resources.size(); i++){
+				if(resources.get(i).getCurrentEndTime() <= job.getStartTime()){
+					resources.get(i).addJob(job);
+					assigned = true;
+				}
+			}
+			
+			//Not enough resources => we need to use another one
+			if(!assigned){
+				Resource res = new Resource();
+				resources.add(res);
+				res.addJob(job);
+			}
+		}
+	}
+	
+	
+	//This method is not optimized as it is not an actual part of the running time ;-)
+	public void printSolution(){
+		System.out.println(resources.size() + "\n");
+		while(!jobStack.isEmpty()){
+			Job job = jobStack.pop();
+			System.out.println(job.getStartTime() + " " + job.getJobTime() + " " + job.getId());
+		}
+	}
+	
+	public static void main(String[] args){
 		
-
 	}
 }
 
